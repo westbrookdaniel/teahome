0b01a86d0b68ac097f4ab5c1b5f6135af4efc69f
westy12dan <westy12dan@gmail.com>
2020-09-30 15:12:38 +0800

    add meta

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Static Site Serving
-app.use(express.static(path.join(__dirname + '/source')))
+app.use(express.static(path.join(__dirname + '/test')))
 
 // Enable CORS for all HTTP methods
 app.use(function(req, res, next) {
