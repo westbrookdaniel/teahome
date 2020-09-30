248dbb91df2951c71640284e7e074dff20dbbbfe
westy12dan <westy12dan@gmail.com>
2020-09-30 15:25:52 +0800

    add more csp

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-   res.header("Content-Security-Policy", "script-src 'self'");
+   res.header("Content-Security-Policy", "default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'");
    
    next();
 });
